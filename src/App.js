f9b8b0784ff4407fe6e83a91f4e0038de77901ab
wahyuda <wahyuda542@smk.belajar.id>
2024-05-03 15:26:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="box">
+        <h1 className="title">Hi, Saya John</h1>
+        <hr></hr>
+        <p className="paragraph">
+          Saya dari Kelas 12 sudah berhasil menginstall React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
